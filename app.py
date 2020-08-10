0de87277bebdd85c51e947bcd668b81697d51f20
quinterojosuej <quinterojosuej@gmail.com>
2020-08-09 21:27:08 -0700

    added most of the styling

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ from torchvision import models
 import torchvision.transforms as transforms
 from PIL import Image
 
+import flask
 from flask import Flask, jsonify, request, render_template, redirect
 
 app = Flask(__name__)
 
 # FLASK_ENV=development FLASK_APP=flaskSimple/app.py flask run
 # to run the app you need to run from the environment
+classify = ['BACTERIA', 'NORMAL', 'VIRUS']
 
 def transform_image(image):
     data_transform = transforms.Compose([
@@ -25,13 +27,12 @@ def transform_image(image):
     to_transform = to_transform.convert('RGB') # if image is grayscale
     return data_transform(to_transform).unsqueeze(0)
 
-def get_prediction(image_bytes):
-    classes = ['BACTERIA', 'NORMAL', 'VIRUS']
+def get_prediction(image_bytes, classes=classify):
     tensor = transform_image(image=image_bytes)
     outputs = model.forward(tensor)
     _, y_hat = outputs.max(1)
     predicted = int(y_hat.item())
-    print(classes[predicted])
+#     print(classes[predicted])
     return classes[predicted] # returns the index!
 
 
@@ -40,22 +41,44 @@ model = torch.load('./model/resnet34_pneumonia_model.pt', map_location=device)
 model.eval()
 
 
-# with open("../pNeumoniaXrays/data/train/NORMAL/IM-0115-0001.jpeg", 'rb') as f:
-#     image_data = f.read()
-#     print("Test image:", get_prediction(image_bytes=image_data), "Done!")
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if flask.request.method == 'GET':
+        return render_template('home.html')
+    if flask.request.method == 'POST':
+        file = request.files['file']
     
-# @app.route('/', methods=['GET'])
-# def home():
-#     return render_template('home.html')
+        if(file.filename.split('.')[-1].lower() != "jpeg" and file.filename.split('.')[-1].lower() != "jpg"): # checks if it a jpeg
+            print('not jpg or jpeg it was a %s' % (file.filename.split('.')[-1].lower()))
+            return redirect('/')
+        print(file.filename)
 
+        image = file.read()
+        class_name = get_prediction(image_bytes=image)
+        print('got image!', class_name)
+        return render_template('home.html', class_name=class_name)
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET'])
+def contatct():
+    return render_template('contact.html')
+    
 # @app.route('/test', methods=['POST']) # tester!
 # def test():
-#     file = request.form['image']
+#     file = request.files['file']
+    
+#     if(file.filename.split('.')[-1].lower() != "jpeg" and file.filename.split('.')[-1].lower() != "jpg"): # checks if it a jpeg
+#         print('not jpg or jpeg it was a %s' % (file.filename.split('.')[-1].lower()))
+#         return redirect('/')
+#     print(file.filename)
     
 #     image = file.read()
 #     class_name = get_prediction(image_bytes=image)
 #     print('got image!')
-#     return redirect('/')
+#     return render_template('results.html', class_name=class_name)
 
 @app.route('/predict', methods=['POST'])
 def predict():
